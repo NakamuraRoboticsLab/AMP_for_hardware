6f7bc064f58c150ed600d7598a44920b836e9b4d
hezepy <89668817@qq.com>
2025-05-06 16:34:54 +0400

    add go2 & retarget

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -33,6 +33,10 @@ from legged_gym.envs.a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .base.legged_robot import LeggedRobot
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .a1.a1_amp_config import A1AMPCfg, A1AMPCfgPPO
+from legged_gym.envs.go2.go2_config import GO2RoughCfg, GO2RoughCfgPPO
+from legged_gym.envs.h1.h1_config import H1RoughCfg, H1RoughCfgPPO
+from legged_gym.envs.h1.h1_env import H1Robot
+from .go2.go2_amp_config import GO2AMPCfg, GO2AMPCfgPPO
 
 
 import os
@@ -41,3 +45,6 @@ from legged_gym.utils.task_registry import task_registry
 
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "a1_amp", LeggedRobot, A1AMPCfg(), A1AMPCfgPPO() )
+task_registry.register( "go2", LeggedRobot, GO2RoughCfg(), GO2RoughCfgPPO())
+task_registry.register( "h1", H1Robot, H1RoughCfg(), H1RoughCfgPPO())
+task_registry.register( "go2_amp", LeggedRobot, GO2AMPCfg(), GO2AMPCfgPPO() )
