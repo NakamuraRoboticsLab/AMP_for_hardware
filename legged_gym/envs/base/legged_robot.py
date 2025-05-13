604b33fe5f47b3fa65ef565297acddebd026aa4f
hezepy <89668817@qq.com>
2025-05-13 15:22:35 +0400

    add height termination

--- legged_gym/envs/base/legged_robot.py
+++ legged_gym/envs/base/legged_robot.py
@@ -187,7 +187,10 @@ class LeggedRobot(BaseTask):
         """
         self.reset_buf = torch.any(torch.norm(self.contact_forces[:, self.termination_contact_indices, :], dim=-1) > 1., dim=1)
         self.time_out_buf = self.episode_length_buf > self.max_episode_length # no terminal reward for time-outs
+        height_cutoff = self.root_states[:, 2] > 1.5
+
         self.reset_buf |= self.time_out_buf
+        self.reset_buf |= height_cutoff
 
     def reset_idx(self, env_ids):
         """ Reset some environments.
